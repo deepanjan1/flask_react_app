124676e078e534b2a60304a5a2bad33671e0e560
Deepanjan De <deepanjan@Deepanjans-MBP.fios-router.home>
2019-09-12 22:02:00 -0400

    created virtual env, added flask server file

---
+++ build/precache-manifest.3c0c6073ae1072ff1fb1fc0b181981ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90847aabde839e3b374d78b351767005",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00a00e4ba80670c8c488",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "6ac12904ffb3dc632dfa",
+    "url": "/static/js/2.90d4b7a7.chunk.js"
+  },
+  {
+    "revision": "00a00e4ba80670c8c488",
+    "url": "/static/js/main.32371e86.chunk.js"
+  },
+  {
+    "revision": "c2861a4d3d06e1def3d5",
+    "url": "/static/js/runtime~main.06a503c9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
